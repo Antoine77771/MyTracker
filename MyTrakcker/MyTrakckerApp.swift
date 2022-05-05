00be3ed7c5ee8c267537cf4b05ec0ade93dd97dc
Antoine <antoineguerregenton@gmail.com>
2022-05-05 14:59:28 +0200

    Initial Commit

---
+++ MyTrakcker/MyTrakckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTrakckerApp.swift
+//  MyTrakcker
+//
+//  Created by Antoine on 05/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyTrakckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
